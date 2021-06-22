06b1c8f4a07b1f935fa3b440fd56ac2bb37af346
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-06-22 12:54:50 -0400

    agregando Algoritmos I

---
+++ Html/algoritmoI/algoritmoI.js
@@ -0,0 +1,81 @@
+/*for(var i=0; i<5;i++){   
+   console.log(i);
+}
+Repuestas:
+0
+1
+2
+3
+4
+
+
+for(var i=0; i<5; i++)
+{
+   i = i + 1;  
+   console.log(i);
+}
+Respuestas:
+1
+3
+5
+
+
+for(var i=0; i<5; i++)
+{
+   i = i + 3; 
+   console.log(i);
+}
+Respuestas:
+3
+7
+
+
+function y(num1, num2){   
+   return num1+num2;
+}
+console.log(y(2,3))
+console.log(y(3,5))
+Respuestas:
+5 
+8
+
+
+function y(num1, num2){
+   console.log(num1);   
+   return num1+num2;
+}
+console.log(y(2,3))
+console.log(y(3,5))
+Respuestas:
+2
+5
+3
+8
+
+a = 15;
+console.log(a);
+function y(a){
+   console.log(a);   
+   return a;
+}
+b = y(10);
+console.log(b);
+Respuestas:
+15
+10
+10
+
+
+a = 15;
+console.log(a);
+function y(a){
+   console.log(a);   
+   return a*2;
+}
+b = y(10);
+console.log(b);
+Respuesta:
+15
+10
+20
+*/
